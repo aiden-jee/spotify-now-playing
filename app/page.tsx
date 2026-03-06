d2422fb627fc4525296e6a57a2bc1dc12d238aa6
Aiden Jee <mjee@ucsd.edu>
2026-03-05 16:27:01 -0800

    added embed flag

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import NowPlaying from "./components/NowPlaying";
 
-export default function Home() {
+export default async function Home(props: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const searchParams = await props.searchParams;
+  const isEmbedded = searchParams.embed === "true";
+
   return (
-    <div className="flex items-center justify-center min-h-dvh p-4">
+    <div
+      className={`min-h-dvh flex items-center justify-center p-4 ${isEmbedded ? "is-embedded min-h-0! p-0!" : ""}`}
+    >
       <NowPlaying />
     </div>
   );
